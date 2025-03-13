9fb0e452d1b1f3659b896303ebafe93e0fae838f
Vyacheslav Tsiganov <v.tsiganov@3205.team>
2025-03-13 11:39:50 +0500

    авторизация ВК

--- app/src/main/java/com/hivislav/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/hivislav/vknewsclient/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.hivislav.vknewsclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.SideEffect
 import com.hivislav.vknewsclient.ui.theme.MainScreen
 import com.hivislav.vknewsclient.ui.theme.VkNewsClientTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             VkNewsClientTheme {
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract(),
+                    onResult = { result ->
+                        when(result) {
+                            is VKAuthenticationResult.Success -> {
+                                Log.d("@@@@", "SUCCESS")
+                            }
+                            is VKAuthenticationResult.Failed -> {
+                                Log.d("@@@@", "FAILED")
+                            }
+                        }
+                    }
+                )
+
+                SideEffect {
+                    launcher.launch(arrayListOf(VKScope.EMAIL))
+                }
+
                 MainScreen()
             }
         }
